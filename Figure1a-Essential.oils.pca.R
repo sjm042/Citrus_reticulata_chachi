f9e9a74bb82d6f547ed6095a30716cbea12ee311
sjm042 <49544799+sjm042@users.noreply.github.com>
2021-10-20 09:23:20 +0800

    Update Figure1a-Essential.oils.pca.R

--- Figure1a-Essential.oils.pca.R
+++ Figure1a-Essential.oils.pca.R
@@ -5,7 +5,7 @@ library(ggplot2)
 library(RColorBrewer)
 library(ggord)
 library(stringr)
-#PCA分析
+#PCA
 oil.pca <- read.table("Essential.oils.csv",sep=",",header = T,row.names = 1)
 oil.pca[is.na(oil.pca)] <- 0
 oil.pca <- oil.pca[,c("α.Pinene","α.Thujene","β.Pinene","α.Terpinene","γ.Terpinene","o.Cymene","δ.Carene")]
@@ -21,15 +21,14 @@ color4pca <- c("#c51b8a","#f03b20","#c994c7","#fec44f","#92C953","#356094","#94B
 pdf("Figure1a.pdf",width = 4.5,height = 3.5)
 ggplot(sample_site, aes(PCA1, PCA2)) +geom_point(aes(shape=region2,fill=region2),size = 3)+
   scale_shape_manual(values = c(25,21,17))+
-  theme_classic()+#去掉背景框
+  theme_classic()+
   geom_vline(xintercept = 0, color = 'black', size = 0.4,lty=2) + 
   geom_hline(yintercept = 0, color = 'black', size = 0.4,lty=2) +
-  scale_color_manual(values = color4pca) + #可在这里修改点的颜色
+  scale_color_manual(values = color4pca) + 
   theme(panel.grid = element_line(color = 'gray', linetype = 2, size = 0.1), 
         panel.background = element_rect(color = 'black', fill = 'transparent'), 
         legend.title=element_blank())+
-  #ggtitle("Essential oils")+
-  stat_ellipse(aes(fill=region2),type="norm",geom="polygon",level=0.8,alpha=0.5)+ 
+ stat_ellipse(aes(fill=region2),type="norm",geom="polygon",level=0.8,alpha=0.5)+ 
   labs(x = paste('PC1 (', round(100 * pca_eig[1], 2), '%)'), y = paste('PC2 (', round(100 * pca_eig[2], 2), '%)')) 
 dev.off()
 
